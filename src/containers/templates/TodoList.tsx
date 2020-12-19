94600786690a6601330d28e7d5a2eed28880bc9d
obr-note <k33mdukb9n@yahoo.co.jp>
2020-12-19 12:23:55 +0900

    Semantic UI Reactの導入

--- src/containers/templates/TodoList.tsx
+++ src/containers/templates/TodoList.tsx
@@ -1,18 +1,28 @@
-import React, { FC } from 'react';
+// import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import TodoList from '../../components/templates/TodoList';
 
 const EnhancedTodoList: FC = () => {
-  // const data: { text: string }[] = [
-  //   { text: 'おはようございます' },
-  //   { text: 'おっはー' },
-  //   { text: 'おはようございます' },
+  const [count, setCount] = useState(0);
+  const [state, setState] = useState<{ id: number; text: string }[]>([]);
+  const add = (): void => {
+    setState([...state, { id: count, text: 'おはよう' }]);
+    setCount((c) => c + 1);
+  };
+  // const data: { id: number; text: string }[] = [
+  //   { id: 1, text: 'おはようございます' },
+  //   { id: 2, text: 'おっはー' },
+  //   { id: 3, text: 'おはようございます' },
   // ];
 
   return (
     <>
-      <TodoList text="おはようございます" />
-      <TodoList text="おっはー" />
-      <TodoList text="おはようございます" />
+      {state.map((item) => (
+        <TodoList key={item.id} text={item.text} />
+      ))}
+      <button onClick={add} type="button">
+        押してね
+      </button>
     </>
   );
 };
