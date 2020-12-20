37ddea88d3b157a87d8dfd0bd080f9e535e5ac9b
obr-note <k33mdukb9n@yahoo.co.jp>
2020-12-20 15:29:22 +0900

    React Router（5系）の導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Redirect, Route, Switch } from 'react-router';
 import EnhancedTodoList from './containers/pages/Home';
+import EnhancedTodoItem from './containers/pages/TodoItem';
 
 const App: React.FC = () => (
-  <>
-    <EnhancedTodoList />
-  </>
+  <Switch>
+    <Route exact path="/" component={EnhancedTodoList} />
+    <Route path="/item/:itemId" component={EnhancedTodoItem} />
+    <Redirect push to="/" />
+  </Switch>
 );
 
 export default App;
---
+++ src/containers/pages/TodoItem.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const EnhancedItem: FC = () => {
+  return (
+    <>
+      <p>ここにいるよ</p>
+    </>
+  );
+};
+
+export default EnhancedItem;
