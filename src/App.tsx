9d600b216f12f30f79d8f0ed18f42a126f9677a7
obr-note <k33mdukb9n@yahoo.co.jp>
2020-12-21 22:23:51 +0900

    reduxの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React, { useEffect } from 'react';
-import { Redirect, Route, Switch, useHistory, useLocation } from 'react-router';
+import { Navigate, Route, Routes, useLocation } from 'react-router';
 import EnhancedTodoList from './containers/pages/Home';
 import EnhancedTodoItem from './containers/pages/TodoItem';
 
 const App: React.FC = () => {
   const { hash, pathname } = useLocation();
-  const { action } = useHistory();
 
   useEffect(() => {
-    if (!hash || action !== 'POP') {
-      window.scrollTo(0, 0);
-    }
-  }, [action, hash, pathname]);
+    if (!hash) window.scrollTo(0, 0);
+  }, [hash, pathname]);
 
   return (
-    <Switch>
-      <Route exact path="/" component={EnhancedTodoList} />
-      <Route path="/item/:itemId" component={EnhancedTodoItem} />
-      <Redirect push to="/" />
-    </Switch>
+    <Routes>
+      <Route path="/" element={<EnhancedTodoList />} />
+      <Route path="/item/:itemId" element={<EnhancedTodoItem />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
--- src/containers/pages/TodoItem.tsx
+++ src/containers/pages/TodoItem.tsx
@@ -1,21 +1,21 @@
 import React, { FC } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const EnhancedItem: FC = () => {
-  const history = useHistory();
-  const { itemId } = useParams<{ itemId: string }>();
+  const navigate = useNavigate();
+  const { itemId } = useParams();
 
   return (
     <>
       <p>ここにいるよ</p>
       <p>{itemId}</p>
-      <button type="button" onClick={() => history.goBack()}>
+      <button type="button" onClick={() => navigate(-1)}>
         戻る
       </button>
-      <button type="button" onClick={() => history.goForward()}>
+      <button type="button" onClick={() => navigate(1)}>
         進む
       </button>
-      <button type="button" onClick={() => history.push('/')}>
+      <button type="button" onClick={() => navigate('/')}>
         トップページへ
       </button>
     </>
