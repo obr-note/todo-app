08259b0ff450fd18a5bd7a38dd3f09423f09359a
obr-note <k33mdukb9n@yahoo.co.jp>
2021-01-17 14:26:16 +0900

    add Firebase storage upload

--- src/components/pages/TestPage.tsx
+++ src/components/pages/TestPage.tsx
@@ -23,12 +23,42 @@ const TestPage: FC<{ firebaseApp: firebase.app.App | undefined }> = ({
     }
   }, [firebaseApp]);
 
+  const btnUploadChange = React.useCallback(
+    (ev: React.ChangeEvent<HTMLInputElement>) => {
+      if (
+        typeof firebaseApp !== 'undefined' &&
+        ev.currentTarget.files !== null
+      ) {
+        const storageRef = firebaseApp.storage().ref();
+        const uploadRef = storageRef.child('upload.png');
+        const f = ev.currentTarget.files[0];
+        uploadRef
+          .put(f)
+          .then(() => {
+            uploadRef
+              .getDownloadURL()
+              .then((url) => {
+                setImgFile(url);
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    [firebaseApp],
+  );
+
   return (
     <>
       <h1>テストページ</h1>
       <Image src={imgFile} size="small" />
       <div id="imgSample" />
       <Button onClick={imgDownload}>Click Here</Button>
+      <input type="file" id="btnUpload" onChange={btnUploadChange} />
     </>
   );
 };
