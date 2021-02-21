dfe6561067501da8925dac72c14676c772ff40e1
obr-note <k33mdukb9n@yahoo.co.jp>
2021-02-21 15:33:31 +0900

    change reducer and actions

--- src/actions.ts
+++ src/actions.ts
@@ -1,38 +1,75 @@
-export const TodoActionType = {
-  ADD: 'ADD',
-  UPDATE: 'UPDATE',
-  DELETE: 'DELETE',
+export const PostActionType = {
+  USER_ADD: 'USER_ADD',
+  POSTS_ADD: 'POSTS_ADD',
+  POSTS_REFRESH: 'POSTS_REFRESH',
+  POPULAR_POSTS_ADD: 'POPULAR_POSTS_ADD',
+  POPULAR_POSTS_REFRESH: 'POPULAR_POSTS_REFRESH',
+  FAVORITE_POSTS_ADD: 'FAVORITE_POSTS_ADD',
+  FAVORITE_POSTS_REFRESH: 'FAVORITE_POSTS_REFRESH',
+  MY_POSTS_ADD: 'MY_POSTS_ADD',
+  MY_POSTS_REFRESH: 'MY_POSTS_REFRESH',
 } as const;
 
 type ValueOf<T> = T[keyof T];
 
-export type TodoAction = {
-  type: ValueOf<typeof TodoActionType>;
-  id?: number;
-  title?: string;
-  body?: string;
-  createdAt?: number;
-  updatedAt?: number;
+export type PostState = {
+  id: string;
+  imageUrl: string;
+  createUser: string;
+  favoritesCount: number;
+  createdAt: number;
+  updatedAt: number;
 };
 
-export const addItem = (title: string, body: string): TodoAction => ({
-  type: TodoActionType.ADD,
-  title,
-  body,
+export type PostAction = {
+  type: ValueOf<typeof PostActionType>;
+  mailAddress?: string;
+  nickname?: string;
+  posts?: PostState[];
+};
+
+export const addUser = (mailAddress: string, nickname: string): PostAction => ({
+  type: PostActionType.USER_ADD,
+  mailAddress,
+  nickname,
 });
 
-export const updateItem = (
-  id: number,
-  title: string,
-  body: string,
-): TodoAction => ({
-  type: TodoActionType.UPDATE,
-  id,
-  title,
-  body,
+export const addPosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.POSTS_ADD,
+  posts,
 });
 
-export const deleteItem = (id: number): TodoAction => ({
-  type: TodoActionType.DELETE,
-  id,
+export const refreshPosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.POSTS_REFRESH,
+  posts,
+});
+
+export const addPopularPosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.POPULAR_POSTS_ADD,
+  posts,
+});
+
+export const refreshPopularPosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.POPULAR_POSTS_REFRESH,
+  posts,
+});
+
+export const addFavoritePosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.FAVORITE_POSTS_ADD,
+  posts,
+});
+
+export const refreshFavoritePosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.FAVORITE_POSTS_REFRESH,
+  posts,
+});
+
+export const addMyPosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.MY_POSTS_ADD,
+  posts,
+});
+
+export const refreshMyPosts = (posts: PostState[]): PostAction => ({
+  type: PostActionType.MY_POSTS_REFRESH,
+  posts,
 });
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,68 +1,99 @@
 import { Reducer } from 'redux';
-import { TodoAction, TodoActionType as Type } from './actions';
+import { PostAction, PostActionType as Type } from './actions';
 
-export type TodoItemState = {
-  title: string;
-  body: string;
+export type PostState = {
+  id: string;
+  imageUrl: string;
+  createUser: string;
+  favoritesCount: number;
   createdAt: number;
   updatedAt: number;
 };
 
-export type TodoState = {
-  idCounter: number;
-  content: { [key: number]: TodoItemState };
+export type PostsState = {
+  user: {
+    mailAddress: string;
+    nickname: string;
+  };
+  posts: PostState[];
+  popularPosts: PostState[];
+  favoritePosts: PostState[];
+  myPosts: PostState[];
 };
 
-export const initialState: TodoState = {
-  idCounter: 0,
-  content: {},
+export const initialState: PostsState = {
+  user: {
+    mailAddress: '',
+    nickname: '',
+  },
+  posts: [],
+  popularPosts: [],
+  favoritePosts: [],
+  myPosts: [],
 };
 
-export const todoReducer: Reducer<TodoState, TodoAction> = (
-  state: TodoState = initialState,
-  action: TodoAction,
-): TodoState => {
+export const todoReducer: Reducer<PostsState, PostAction> = (
+  state: PostsState = initialState,
+  action: PostAction,
+): PostsState => {
   switch (action.type) {
-    case Type.ADD:
+    case Type.USER_ADD:
       return {
         ...state,
-        idCounter: state.idCounter + 1,
-        content: {
-          ...state.content,
-          [state.idCounter + 1]: {
-            title: action.title || '',
-            body: action.body || '',
-            createdAt: Date.now(),
-            updatedAt: Date.now(),
-          },
+        user: {
+          mailAddress: action.mailAddress || '',
+          nickname: action.nickname || '',
         },
       };
-    case Type.UPDATE:
-      if (typeof action.id === 'undefined') {
-        return state;
-      }
 
+    case Type.POSTS_ADD:
       return {
         ...state,
-        content: {
-          ...state.content,
-          [action.id]: {
-            title: action.title || '',
-            body: action.body || '',
-            createdAt: state.content[action.id].createdAt,
-            updatedAt: Date.now(),
-          },
-        },
+        posts: [...state.posts, ...(action.posts || [])],
       };
-    case Type.DELETE:
-      if (typeof action.id === 'undefined') {
-        return state;
-      }
-      // eslint-disable-next-line
-      const newState = state;
-      delete newState.content[action.id];
 
-      return newState;
+    case Type.POSTS_REFRESH:
+      return {
+        ...state,
+        posts: [...(action.posts || [])],
+      };
+
+    case Type.POPULAR_POSTS_ADD:
+      return {
+        ...state,
+        posts: [...state.posts, ...(action.posts || [])],
+      };
+
+    case Type.POPULAR_POSTS_REFRESH:
+      return {
+        ...state,
+        posts: [...(action.posts || [])],
+      };
+
+    case Type.FAVORITE_POSTS_ADD:
+      return {
+        ...state,
+        posts: [...state.posts, ...(action.posts || [])],
+      };
+
+    case Type.FAVORITE_POSTS_REFRESH:
+      return {
+        ...state,
+        posts: [...(action.posts || [])],
+      };
+
+    case Type.MY_POSTS_ADD:
+      return {
+        ...state,
+        posts: [...state.posts, ...(action.posts || [])],
+      };
+
+    case Type.MY_POSTS_REFRESH:
+      return {
+        ...state,
+        posts: [...(action.posts || [])],
+      };
+
     default: {
       const _: never = action.type;
 
